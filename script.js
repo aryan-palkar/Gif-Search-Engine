fc09aebe653412b9cccb191950c618b1ae75ab9a
aryan-palkar <aryan.palkar02@gmail.com>
2021-09-16 16:57:14 +0530

    Search Bar Working, (small changes to css)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("linked!");
+// Search bar implementation
+function storeInput(){
+    let inputValue = document.querySelector(".search-bar").value;
+    console.log(inputValue);
+}
+
+document.querySelector(".search-button")
+.addEventListener("click", storeInput);
+
+document.querySelector(".search-bar")
+.addEventListener("keyup", function(e){
+    if(e.key == "Enter") 
+    {
+        storeInput();
+    }
+});
+
+// API call
+
